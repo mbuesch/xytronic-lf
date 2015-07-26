7d89ebe56c2680c51c9ae4c05b4edbcc7aa755d8
Michael Buesch <m@bues.ch>
2015-07-26 21:39:17 +0200

    tests: Extend check macro
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/tests/test.h
+++ firmware/tests/test.h
@@ -1,5 +1,17 @@
-#define CHECK(cond)	do {			\
-		if (!(cond)) {			\
-			exit(1);		\
-		}				\
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define DO_STRINGIFY(x)	#x
+#define STRINGIFY(x)	DO_STRINGIFY(x)
+
+
+#define CHECK(cond)	do {						\
+		if (!(cond)) {						\
+			fprintf(stderr,					\
+				"Test condition at %s:%d failed:\n"	\
+				"    %s\n",				\
+				__FILE__, __LINE__, STRINGIFY(cond));	\
+			exit(1);					\
+		}							\
 	} while (0)
