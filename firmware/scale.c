6e90df2f8c914d5c83e420e34ab47d745ec3e71a
Michael Buesch <m@bues.ch>
2015-08-09 14:12:16 +0200

    scale: Add rescale and rewrite impl2
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/scale.c
+++ firmware/scale.c
@@ -23,10 +23,13 @@
 
 
 #ifndef SCALE_IMPL
-# define SCALE_IMPL	1
+# define SCALE_IMPL	2
 #endif
 #ifndef UNSCALE_IMPL
-# define UNSCALE_IMPL	1
+# define UNSCALE_IMPL	2
+#endif
+#ifndef RESCALE_IMPL
+# define RESCALE_IMPL	2
 #endif
 
 
@@ -57,9 +60,9 @@ fixpt_t scale(int16_t raw, int16_t raw_lo, int16_t raw_hi,
 			    int_to_fixpt_big(tmp));
 
 	return fixpt_deflate(ret);
+
 #elif SCALE_IMPL == 2
-	fixpt_big_t a, b, c;
-	int16_t tmp;
+	fixpt_big_t a, b, c, d, ret;
 
 	/*         (phys_hi - phys_lo) * (raw - raw_lo)
 	 *  ret = -------------------------------------- + phys_lo
@@ -69,15 +72,15 @@ fixpt_t scale(int16_t raw, int16_t raw_lo, int16_t raw_hi,
 	a = fixpt_big_sub(fixpt_inflate(phys_hi),
 			  fixpt_inflate(phys_lo));
 
-	tmp = (int16_t)(raw - raw_lo);
-	b = fixpt_big_mul(a, int_to_fixpt_big(tmp));
+	b = int_to_fixpt_big(raw - raw_lo);
 
-	tmp = (int16_t)(raw_hi - raw_lo);
-	c = fixpt_big_div(b, int_to_fixpt_big(tmp));
+	c = int_to_fixpt_big(raw_hi - raw_lo);
 
-	c = fixpt_big_add(c, phys_lo);
+	d = fixpt_big_mul_div(a, b, c);
 
-	return fixpt_deflate(c);
+	ret = fixpt_big_add(d, fixpt_inflate(phys_lo));
+
+	return fixpt_deflate(ret);
 #else
 # error
 #endif
@@ -108,25 +111,81 @@ int16_t unscale(fixpt_t phys, fixpt_t phys_lo, fixpt_t phys_hi,
 	return (int16_t)fixpt_big_to_int(c);
 
 #elif UNSCALE_IMPL == 2
-	fixpt_big_t a, b, c;
+	fixpt_big_t a, b, c, d, ret;
 
 	/*         (raw_hi - raw_lo) * (phys - phys_lo)
 	 *  ret = -------------------------------------- + raw_lo
 	 *                  phys_hi - phys_lo
 	 */
 
-	a = fixpt_big_mul(int_to_fixpt_big(raw_hi - raw_lo),
-			  fixpt_big_sub(fixpt_inflate(phys),
-					fixpt_inflate(phys_lo)));
+	a = int_to_fixpt_big(raw_hi - raw_lo);
 
-	b = fixpt_big_sub(fixpt_inflate(phys_hi),
+	b = fixpt_big_sub(fixpt_inflate(phys),
 			  fixpt_inflate(phys_lo));
 
-	c = fixpt_big_div(a, fixpt_deflate(b));
+	c = fixpt_big_sub(fixpt_inflate(phys_hi),
+			  fixpt_inflate(phys_lo));
 
-	c = fixpt_big_add(c, int_to_fixpt_big(raw_lo));
+	d = fixpt_big_mul_div(a, b, c);
 
-	return (int16_t)fixpt_to_int(fixpt_deflate(c));
+	ret = fixpt_big_add(d, int_to_fixpt_big(raw_lo));
+
+	return (int16_t)fixpt_to_int(fixpt_deflate(ret));
+#else
+# error
+#endif
+}
+
+fixpt_t rescale(fixpt_t phys0, fixpt_t phys0_lo, fixpt_t phys0_hi,
+		fixpt_t phys1_lo, fixpt_t phys1_hi)
+{
+#if RESCALE_IMPL == 1
+	fixpt_big_t a, b, c, d, ret;
+
+	/*         phys1_lo * (phys0_hi - phys0) + phys1_hi * (phys0 - phys0_lo)
+	 *  ret = ---------------------------------------------------------------
+	 *                              phys0_hi - phys0_lo
+	 */
+
+	a = fixpt_big_sub(fixpt_inflate(phys0_hi),
+			  fixpt_inflate(phys0));
+	a = fixpt_big_mul(fixpt_inflate(phys1_lo), a);
+
+	b = fixpt_big_sub(fixpt_inflate(phys0),
+			  fixpt_inflate(phys0_lo));
+	b = fixpt_big_mul(fixpt_inflate(phys1_hi), b);
+
+	c = fixpt_big_add(a, b);
+
+	d = fixpt_big_sub(fixpt_inflate(phys0_hi),
+			  fixpt_inflate(phys0_lo));
+
+	ret = fixpt_big_div(c, d);
+
+	return fixpt_deflate(ret);
+
+#elif RESCALE_IMPL == 2
+	fixpt_big_t a, b, c, d, ret;
+
+	/*         (phys1_hi - phys1_lo) * (phys0 - phys0_lo)
+	 *  ret = -------------------------------------------- + phys1_lo
+	 *                    phys0_hi - phys0_lo
+	 */
+
+	a = fixpt_big_sub(fixpt_inflate(phys1_hi),
+			  fixpt_inflate(phys1_lo));
+
+	b = fixpt_big_sub(fixpt_inflate(phys0),
+			  fixpt_inflate(phys0_lo));
+
+	c = fixpt_big_sub(fixpt_inflate(phys0_hi),
+			  fixpt_inflate(phys0_lo));
+
+	d = fixpt_big_mul_div(a, b, c);
+
+	ret = fixpt_big_add(d, fixpt_inflate(phys1_lo));
+
+	return fixpt_deflate(ret);
 #else
 # error
 #endif
--- firmware/scale.h
+++ firmware/scale.h
@@ -1,5 +1,5 @@
-#ifndef SCALE_UNSCALE_H_
-#define SCALE_UNSCALE_H_
+#ifndef SCALE_H_
+#define SCALE_H_
 
 #include <stdint.h>
 
@@ -12,4 +12,7 @@ fixpt_t scale(int16_t raw, int16_t raw_lo, int16_t raw_hi,
 int16_t unscale(fixpt_t phys, fixpt_t phys_lo, fixpt_t phys_hi,
 		int16_t raw_lo, int16_t raw_hi);
 
-#endif /* SCALE_UNSCALE_H_ */
+fixpt_t rescale(fixpt_t phys0, fixpt_t phys0_lo, fixpt_t phys0_hi,
+		fixpt_t phys1_lo, fixpt_t phys1_hi);
+
+#endif /* SCALE_H_ */
