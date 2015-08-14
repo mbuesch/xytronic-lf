9448d5a089e874729008ba0eae618e30d252e437
Michael Buesch <m@bues.ch>
2015-08-14 20:00:01 +0200

    tests: Add more check macros
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/tests/test.h
+++ firmware/tests/test.h
@@ -9,9 +9,44 @@
 #define CHECK(cond)	do {						\
 		if (!(cond)) {						\
 			fprintf(stderr,					\
-				"Test condition at %s:%d failed:\n"	\
-				"    %s\n",				\
-				__FILE__, __LINE__, STRINGIFY(cond));	\
+				"\nTest condition\n"			\
+				"    %s\n"				\
+				"at %s:%d\n"				\
+				"FAILED\n\n",				\
+				STRINGIFY(cond),			\
+				__FILE__, __LINE__);			\
+			exit(1);					\
+		}							\
+	} while (0)
+
+#define CHECK_EQ_II(a, b)	do {					\
+		if (!((a) == (b))) {					\
+			fprintf(stderr,					\
+				"\nTest condition\n"			\
+				"    %s == %s\n"			\
+				"evaluated as\n"			\
+				"    %d == %d\n"			\
+				"at %s:%d\n"				\
+				"FAILED\n\n",				\
+				STRINGIFY(a), STRINGIFY(b),		\
+				(int)(a), (int)(b),			\
+				__FILE__, __LINE__);			\
+			exit(1);					\
+		}							\
+	} while (0)
+
+#define CHECK_EQ_UU(a, b)	do {					\
+		if (!((a) == (b))) {					\
+			fprintf(stderr,					\
+				"\nTest condition\n"			\
+				"    %s == %s\n"			\
+				"evaluated as\n"			\
+				"    %u == %u\n"			\
+				"at %s:%d\n"				\
+				"FAILED\n\n",				\
+				STRINGIFY(a), STRINGIFY(b),		\
+				(unsigned int)(a), (unsigned int)(b),	\
+				__FILE__, __LINE__);			\
 			exit(1);					\
 		}							\
 	} while (0)
