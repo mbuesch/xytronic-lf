3c6c5ac5f88d56275702d158635c7700ea2bb96a
Michael Buesch <m@bues.ch>
2015-09-26 16:28:10 +0200

    Set fuse information in README
    Signed-off-by: Michael Buesch <m@bues.ch>

--- makerelease.sh
+++ makerelease.sh
@@ -36,10 +36,20 @@ hook_pre_archives()
 	# Build the hex files.
 	make -C "$2/firmware" all
 	make -C "$2/firmware" clean
-	# Move the hex files.
 	mv "$2"/firmware/*.hex "$2"
+
 	# Move the documentation.
 	mv "$2"/schematics-lf1600/lf1600.pdf "$2"/schematics-lf1600.pdf
+
+	# Update the README
+	local makefile="$2/firmware/Makefile"
+	local lfuse="$(grep -e 'LFUSE[[:space:]]*:=' "$makefile" | head -n1 | cut -d'=' -f2 | tr -d '[[:space:]]')"
+	local hfuse="$(grep -e 'HFUSE[[:space:]]*:=' "$makefile" | head -n1 | cut -d'=' -f2 | tr -d '[[:space:]]')"
+	local efuse="$(grep -e 'EFUSE[[:space:]]*:=' "$makefile" | head -n1 | cut -d'=' -f2 | tr -d '[[:space:]]')"
+	sed -i -e 's/%%LFUSE%%/'"$lfuse"'/g' \
+	       -e 's/%%HFUSE%%/'"$hfuse"'/g' \
+	       -e 's/%%EFUSE%%/'"$efuse"'/g' \
+	       "$2/README"
 }
 
 project=xytronic-lf
