65183f435b2c95d25002114b1669715161a3e4b6
Michael Buesch <m@bues.ch>
2015-11-01 12:09:52 +0100

    tests/fixpt: Add decimal tests
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/tests/test_fixpt.c
+++ firmware/tests/test_fixpt.c
@@ -1,5 +1,7 @@
 #include "test.h"
 #include "fixpt.h"
+
+#include "util.c"
 #include "fixpt.c"
 
 
@@ -34,6 +36,32 @@ static void check_common(void)
 	CHECK(fixpt_to_int(float_to_fixpt(-0.4f)) == 0);
 	CHECK(fixpt_to_int(float_to_fixpt(-0.5f)) == -1);
 	CHECK(fixpt_to_int(float_to_fixpt(-0.6f)) == -1);
+
+	/* Check decimal functions */
+
+	CHECK(fixpt_get_int_part(float_to_fixpt(0.0f)) == 0);
+	CHECK(fixpt_get_int_part(float_to_fixpt(1.0f)) == 1);
+	CHECK(fixpt_get_int_part(float_to_fixpt(1.5f)) == 1);
+	CHECK(fixpt_get_int_part(float_to_fixpt(2.5f)) == 2);
+	CHECK(fixpt_get_int_part(float_to_fixpt(-1.0f)) == -1);
+	CHECK(fixpt_get_int_part(float_to_fixpt(-1.5f)) == -1);
+	CHECK(fixpt_get_int_part(float_to_fixpt(-2.5f)) == -2);
+
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(1.25f), 2) == 25);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(-1.25f), 2) == 25);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(1.25f), 1) == 3);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(1.225f), 1) == 2);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(-1.25f), 1) == 3);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(-1.225f), 1) == 2);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(1.125f), 1) == 1);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(1.125f), 2) == 13);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(1.125f), 3) == 125);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(-1.125f), 1) == 1);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(-1.125f), 2) == 13);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(-1.125f), 3) == 125);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(1.0f), 2) == 0);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(-1.0f), 2) == 0);
+	CHECK(fixpt_get_dec_fract(float_to_fixpt(0.0f), 2) == 0);
 }
 
 int main(void)
