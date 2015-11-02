9e29c2f6358a9582b6b314d26098a93dc92e0525
Michael Buesch <m@bues.ch>
2015-11-02 21:53:45 +0100

    menu: Add k config
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/menu.c
+++ firmware/menu.c
@@ -27,11 +27,13 @@
 #include "controller_current.h"
 #include "debug_uart.h"
 #include "calib_current.h"
+#include "settings.h"
 
 #include <string.h>
 
 
 #define MENU_SETTEMP_TIMEOUT	3000
+#define MENU_KCONF_PRE_TIMEOUT	1000
 
 
 enum menu_state {
@@ -39,8 +41,11 @@ enum menu_state {
 	MENU_SETTEMP,		/* Temperature setpoint. */
 	MENU_DEBUG,		/* Debug enable. */
 	MENU_CALIB,		/* Current calibration. */
+	MENU_KP_PRE,		/* Temp KP config */
 	MENU_KP,		/* Temp KP config */
+	MENU_KI_PRE,		/* Temp KI config */
 	MENU_KI,		/* Temp KI config */
+	MENU_KD_PRE,		/* Temp KD config */
 	MENU_KD,		/* Temp KD config */
 };
 
@@ -75,7 +80,8 @@ static struct menu_context menu;
 
 static void int_to_ascii_align_right(char *buf, uint8_t align_to_digit,
 				     int16_t val,
-				     int16_t min_val, int16_t max_val)
+				     int16_t min_val, int16_t max_val,
+				     char fill_char)
 {
 	uint8_t i;
 
@@ -86,7 +92,7 @@ static void int_to_ascii_align_right(char *buf, uint8_t align_to_digit,
 		while (buf[align_to_digit] == '\0') {
 			for (i = align_to_digit; i > 0; i--)
 				buf[i] = buf[i - 1];
-			buf[0] = ' ';
+			buf[0] = fill_char;
 		}
 	}
 }
@@ -94,11 +100,14 @@ static void int_to_ascii_align_right(char *buf, uint8_t align_to_digit,
 static void menu_update_display(void)
 {
 	char disp[6];
-	fixpt_t temp_fixpt;
+	fixpt_t temp_fixpt, k;
 	int16_t temp_int;
 	uint8_t displayed_error;
 	bool displayed_heating;
+	enum contrtemp_boostmode boost_mode;
+	int16_t ipart, fpart;
 
+	boost_mode = contrtemp_get_boost_mode();
 	displayed_error = menu.displayed_error;
 	displayed_heating = menu.displayed_heating;
 	disp[0] = '\0';
@@ -106,8 +115,9 @@ static void menu_update_display(void)
 	if (displayed_error) {
 		strcpy_P(disp, PSTR("Err"));
 		int_to_ascii_align_right(disp + 3, 0,
-					 displayed_error, 0, 9);
+					 displayed_error, 0, 9, ' ');
 		disp[5] = '\0';
+		displayed_heating = false;
 	} else {
 		switch (menu.state) {
 		case MENU_CURTEMP:
@@ -116,8 +126,9 @@ static void menu_update_display(void)
 			int_to_ascii_align_right(disp, 2,
 						 temp_int,
 						 (int16_t)CONTRTEMP_NEGLIM,
-						 (int16_t)CONTRTEMP_POSLIM);
-			switch (contrtemp_get_boost_mode()) {
+						 (int16_t)CONTRTEMP_POSLIM,
+						 ' ');
+			switch (boost_mode) {
 			case TEMPBOOST_NORMAL:
 				strcpy_P(disp + 3, PSTR("C."));
 				break;
@@ -138,7 +149,8 @@ static void menu_update_display(void)
 			int_to_ascii_align_right(disp, 2,
 						 temp_int,
 						 (int16_t)CONTRTEMP_NEGLIM,
-						 (int16_t)CONTRTEMP_POSLIM);
+						 (int16_t)CONTRTEMP_POSLIM,
+						 ' ');
 			strcpy_P(disp + 3, PSTR("S"));
 			break;
 		case MENU_DEBUG:
@@ -151,30 +163,62 @@ static void menu_update_display(void)
 				break;
 			strcpy_P(disp, PSTR("CAL"));
 			break;
+		case MENU_KP_PRE:
+			if (!CONF_KCONF)
+				break;
+			strcpy_P(disp, PSTR("CO. P"));
+			displayed_heating = false;
+			break;
 		case MENU_KP:
 			if (!CONF_KCONF)
 				break;
-			//TODO
-			strcpy_P(disp, PSTR("CO. P"));
+			k = get_settings()->temp_k[boost_mode].kp;
+			ipart = (int16_t)clamp(fixpt_get_int_part(k), 0, 99);
+			fpart = (int16_t)fixpt_get_dec_fract(k, 2);
+			int_to_ascii_align_right(&disp[0], 1, ipart, 0, 99, ' ');
+			disp[2] = '.';
+			int_to_ascii_align_right(&disp[3], 1, fpart, 0, 99, '0');
+			displayed_heating = false;
+			break;
+		case MENU_KI_PRE:
+			if (!CONF_KCONF)
+				break;
+			strcpy_P(disp, PSTR("CO. I"));
+			displayed_heating = false;
 			break;
 		case MENU_KI:
 			if (!CONF_KCONF)
 				break;
-			//TODO
-			strcpy_P(disp, PSTR("CO. I"));
+			k = get_settings()->temp_k[boost_mode].ki;
+			ipart = (int16_t)clamp(fixpt_get_int_part(k), 0, 99);
+			fpart = (int16_t)fixpt_get_dec_fract(k, 2);
+			int_to_ascii_align_right(&disp[0], 1, ipart, 0, 99, ' ');
+			disp[2] = '.';
+			int_to_ascii_align_right(&disp[3], 1, fpart, 0, 99, '0');
+			displayed_heating = false;
+			break;
+		case MENU_KD_PRE:
+			if (!CONF_KCONF)
+				break;
+			strcpy_P(disp, PSTR("CO. D"));
+			displayed_heating = false;
 			break;
 		case MENU_KD:
 			if (!CONF_KCONF)
 				break;
-			//TODO
-			strcpy_P(disp, PSTR("CO. D"));
+			k = get_settings()->temp_k[boost_mode].kd;
+			ipart = (int16_t)clamp(fixpt_get_int_part(k), 0, 99);
+			fpart = (int16_t)fixpt_get_dec_fract(k, 2);
+			int_to_ascii_align_right(&disp[0], 1, ipart, 0, 99, ' ');
+			disp[2] = '.';
+			int_to_ascii_align_right(&disp[3], 1, fpart, 0, 99, '0');
+			displayed_heating = false;
 			break;
 		}
 	}
 
 	/* Show the 'is heating' dot. */
-	display_force_dp(2, true,
-			 (displayed_heating && !displayed_error));
+	display_force_dp(2, true, displayed_heating);
 	/* Update the display content. */
 	display_show(disp);
 }
@@ -194,7 +238,7 @@ static enum menu_state next_menu_state(enum menu_state cur_state)
 		if (CONF_CALIB)
 			return MENU_CALIB;
 		if (CONF_KCONF)
-			return MENU_KP;
+			return MENU_KP_PRE;
 		return MENU_CURTEMP;
 	case MENU_SETTEMP:
 		return MENU_CURTEMP;
@@ -202,15 +246,21 @@ static enum menu_state next_menu_state(enum menu_state cur_state)
 		if (CONF_CALIB)
 			return MENU_CALIB;
 		if (CONF_KCONF)
-			return MENU_KP;
+			return MENU_KP_PRE;
 		return MENU_CURTEMP;
 	case MENU_CALIB:
 		if (CONF_KCONF)
-			return MENU_KP;
+			return MENU_KP_PRE;
 		return MENU_CURTEMP;
+	case MENU_KP_PRE:
+		return MENU_KP;
 	case MENU_KP:
+		return MENU_KI_PRE;
+	case MENU_KI_PRE:
 		return MENU_KI;
 	case MENU_KI:
+		return MENU_KD_PRE;
+	case MENU_KD_PRE:
 		return MENU_KD;
 	case MENU_KD:
 		return MENU_CURTEMP;
@@ -222,6 +272,27 @@ static enum menu_state next_menu_state(enum menu_state cur_state)
 static void menu_set_state(enum menu_state new_state)
 {
 	menu.state = new_state;
+
+	switch (new_state) {
+	case MENU_SETTEMP:
+		timer_arm(&menu.timeout, MENU_SETTEMP_TIMEOUT);
+		break;
+	case MENU_KP_PRE:
+	case MENU_KI_PRE:
+	case MENU_KD_PRE:
+		if (!CONF_KCONF)
+			break;
+		timer_arm(&menu.timeout, MENU_KCONF_PRE_TIMEOUT);
+		break;
+	case MENU_CURTEMP:
+	case MENU_DEBUG:
+	case MENU_CALIB:
+	case MENU_KP:
+	case MENU_KI:
+	case MENU_KD:
+		break;
+	}
+
 	menu_request_display_update();
 }
 
@@ -230,23 +301,66 @@ static void menu_set_next_state(void)
 	menu_set_state(next_menu_state(menu.state));
 }
 
+static fixpt_t fixpt_add_limited(fixpt_t a, fixpt_t b,
+				 fixpt_t lo_lim, fixpt_t hi_lim)
+{
+	fixpt_t ret;
+
+	ret = fixpt_add(a, b);
+	if (ret > hi_lim)
+		ret = hi_lim;
+	if (ret < lo_lim)
+		ret = lo_lim;
+
+	return ret;
+}
+
 static void settemp_ramp_handler(bool up)
 {
 	fixpt_t setpoint;
 
-	setpoint = contrtemp_get_setpoint();
-	if (up) {
-		setpoint = fixpt_add(setpoint, float_to_fixpt(1.0));
-	} else {
-		setpoint = fixpt_sub(setpoint, float_to_fixpt(1.0));
-	}
-	if (setpoint > float_to_fixpt(CONTRTEMP_POSLIM))
-		setpoint = float_to_fixpt(CONTRTEMP_POSLIM);
-	if (setpoint < float_to_fixpt(CONTRTEMP_NEGLIM))
-		setpoint = float_to_fixpt(CONTRTEMP_NEGLIM);
+	setpoint = fixpt_add_limited(contrtemp_get_setpoint(),
+				     (up ? float_to_fixpt(1.0) :
+					   float_to_fixpt(-1.0)),
+				     float_to_fixpt(CONTRTEMP_NEGLIM),
+				     float_to_fixpt(CONTRTEMP_POSLIM));
 	contrtemp_set_setpoint(setpoint);
+}
 
-	menu_request_display_update();
+static void kconf_kp_ramp_handler(bool up)
+{
+	fixpt_t *k;
+
+	k = &(get_settings()->temp_k[contrtemp_get_boost_mode()].kp);
+	*k = fixpt_add_limited(*k, (up ? float_to_fixpt(0.03125) :
+					 float_to_fixpt(-0.03125)),
+			       float_to_fixpt(0.0),
+			       float_to_fixpt(99.0));
+	store_settings();
+}
+
+static void kconf_ki_ramp_handler(bool up)
+{
+	fixpt_t *k;
+
+	k = &(get_settings()->temp_k[contrtemp_get_boost_mode()].ki);
+	*k = fixpt_add_limited(*k, (up ? float_to_fixpt(0.03125) :
+					 float_to_fixpt(-0.03125)),
+			       float_to_fixpt(0.0),
+			       float_to_fixpt(99.0));
+	store_settings();
+}
+
+static void kconf_kd_ramp_handler(bool up)
+{
+	fixpt_t *k;
+
+	k = &(get_settings()->temp_k[contrtemp_get_boost_mode()].kd);
+	*k = fixpt_add_limited(*k, (up ? float_to_fixpt(0.03125) :
+					 float_to_fixpt(-0.03125)),
+			       float_to_fixpt(0.0),
+			       float_to_fixpt(99.0));
+	store_settings();
 }
 
 static void start_ramping(enum ramp_state ramp, ramp_handler_t handler)
@@ -281,7 +395,6 @@ static void menu_button_handler(enum button_id button,
 						RAMP_DOWN : RAMP_UP,
 					      settemp_ramp_handler);
 				menu_set_state(MENU_SETTEMP);
-				timer_arm(&menu.timeout, MENU_SETTEMP_TIMEOUT);
 				break;
 			}
 		}
@@ -354,44 +467,68 @@ static void menu_button_handler(enum button_id button,
 			}
 		}
 		break;
+	case MENU_KP_PRE:
+		break;
 	case MENU_KP:
 		if (!CONF_KCONF)
 			break;
 		if (bstate == BSTATE_POSEDGE) {
+			if (button == BUTTON_MINUS ||
+			    button == BUTTON_PLUS) {
+				start_ramping((button == BUTTON_MINUS) ?
+						RAMP_DOWN : RAMP_UP,
+					      kconf_kp_ramp_handler);
+			}
 		}
 		if (bstate == BSTATE_NEGEDGE) {
+			stop_ramping();
 			if (button == BUTTON_SET) {
 				menu_set_next_state();
 				break;
 			}
 		}
-		//TODO
+		break;
+	case MENU_KI_PRE:
 		break;
 	case MENU_KI:
 		if (!CONF_KCONF)
 			break;
 		if (bstate == BSTATE_POSEDGE) {
+			if (button == BUTTON_MINUS ||
+			    button == BUTTON_PLUS) {
+				start_ramping((button == BUTTON_MINUS) ?
+						RAMP_DOWN : RAMP_UP,
+					      kconf_ki_ramp_handler);
+			}
 		}
 		if (bstate == BSTATE_NEGEDGE) {
+			stop_ramping();
 			if (button == BUTTON_SET) {
 				menu_set_next_state();
 				break;
 			}
 		}
-		//TODO
+		break;
+	case MENU_KD_PRE:
 		break;
 	case MENU_KD:
 		if (!CONF_KCONF)
 			break;
 		if (bstate == BSTATE_POSEDGE) {
+			if (button == BUTTON_MINUS ||
+			    button == BUTTON_PLUS) {
+				start_ramping((button == BUTTON_MINUS) ?
+						RAMP_DOWN : RAMP_UP,
+					      kconf_kd_ramp_handler);
+			}
 		}
 		if (bstate == BSTATE_NEGEDGE) {
+			stop_ramping();
 			if (button == BUTTON_SET) {
 				menu_set_next_state();
 				break;
 			}
 		}
-		//TODO
 		break;
 	}
 }
@@ -411,23 +548,16 @@ void menu_work(void)
 	case MENU_KI:
 	case MENU_KD:
 		break;
+	case MENU_KP_PRE:
+	case MENU_KI_PRE:
+	case MENU_KD_PRE:
 	case MENU_SETTEMP:
 		if (timer_expired(&menu.timeout)) {
-			menu_set_state(MENU_CURTEMP);
+			menu_set_next_state();
 			stop_ramping();
 			break;
 		}
 		break;
-	}
-
-	/* Generic ramping handler. */
-	if (menu.ramp != RAMP_NONE) {
-		if (timer_expired(&menu.ramp_timer)) {
-			menu.ramp_handler(menu.ramp == RAMP_UP);
-			timer_add(&menu.ramp_timer, menu.ramp_period);
-			if (menu.ramp_period > RAMP_MIN_PERIOD_MS)
-				menu.ramp_period /= 2;
-		}
 	}
 
 	/* Evaluate error conditions. */
@@ -446,6 +576,21 @@ void menu_work(void)
 	if (heating != menu.displayed_heating) {
 		menu.displayed_heating = heating;
 		menu.display_update_requested = true;
+	}
+
+	/* Generic ramping handler. */
+	if (menu.ramp != RAMP_NONE) {
+		if (menu.displayed_error) {
+			stop_ramping();
+		} else {
+			if (timer_expired(&menu.ramp_timer)) {
+				menu.ramp_handler(menu.ramp == RAMP_UP);
+				menu_request_display_update();
+				timer_add(&menu.ramp_timer, menu.ramp_period);
+				if (menu.ramp_period > RAMP_MIN_PERIOD_MS)
+					menu.ramp_period /= 2;
+			}
+		}
 	}
 
 	/* Update the display, if requested. */
