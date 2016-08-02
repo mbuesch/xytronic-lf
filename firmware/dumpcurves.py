b3c6e811bdb7e3e301627af9b621bde3584a71d9
Michael Buesch <m@bues.ch>
2016-08-02 17:34:45 +0200

    Add debug tool description
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/dumpcurves.py
+++ firmware/dumpcurves.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python3
+#
+# Xytronic LF-1600
+# Debug interface tool
+#
+# Copyright (c) 2015-2016 Michael Buesch <m@bues.ch>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 
 import sys
 import serial
 
+
+if len(sys.argv) != 2 or "-h" in sys.argv or "--help" in sys.argv:
+	print("Usage: dumpcurves.py /dev/ttyUSB0 output.csv")
+	print("")
+	print("/dev/ttyUSB0 is the serial device where the data is read from.")
+	print("output.csv is the output file.")
+	sys.exit(1)
 
 inDev = sys.argv[1]
 outFile = sys.argv[2]
