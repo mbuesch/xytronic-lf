e8764369250a03f19bd16d582efcf4d4a022b332
Michael Buesch <m@bues.ch>
2017-09-07 20:03:35 +0200

    avrmakelib: Clean dasm
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/avrmakelib.mk
+++ firmware/avrmakelib.mk
@@ -360,6 +360,7 @@ clean:
 		$(BOOT_OBJ_DIR) $(BOOT_DEP_DIR) \
 		$(BIN) $(BOOT_BIN) \
 		$(MAP) $(BOOT_MAP) \
+		$(DASM) $(BOOT_DASM) \
 		*.pyc *.pyo __pycache__ \
 		$(GEN_SRCS) $(BOOT_GEN_SRCS) \
 		$(CLEAN_FILES)
