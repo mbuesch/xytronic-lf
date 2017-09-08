c4c2475e65bec072b18387d0b802717646fa509f
Michael Buesch <m@bues.ch>
2017-09-08 21:34:54 +0200

    pwn: Reduce PWM frequency
    Signed-off-by: Michael Buesch <m@bues.ch>

--- firmware/pwm_current.c
+++ firmware/pwm_current.c
@@ -76,11 +76,11 @@ void pwmcurr_init(void)
 	TIMSK1 &= (uint8_t)~((1 << ICIE1) |
 			     (1 << OCIE1B) | (1 << OCIE1A) |
 			     (1 << TOIE1));
-	/* Enable timer: Fast PWM (Mode 14), OC1A clr/set, PS 64 */
+	/* Enable timer: Fast PWM (Mode 14), OC1A clr/set, PS 256 */
 	TCCR1A = (1 << COM1A1) | (0 << COM1A0) |
 		 (0 << COM1B1) | (0 << COM1B0) |
 		 (1 << WGM11) | (0 << WGM10);
 	TCCR1C = 0;
 	TCCR1B = (1 << WGM13) | (1 << WGM12) |
-		 (0 << CS12) | (1 << CS11) | (1 << CS10);
+		 (1 << CS12) | (0 << CS11) | (0 << CS10);
 }
