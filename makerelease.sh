862705674b48bba234f8f58cc5940f83f4e11178
Michael Buesch <m@bues.ch>
2025-04-09 20:36:21 +0200

    Update release script

--- makerelease.sh
+++ makerelease.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-srcdir="$(dirname "$0")"
-[ "$(echo "$srcdir" | cut -c1)" = '/' ] || srcdir="$PWD/$srcdir"
-
-die() { echo "$*"; exit 1; }
+srcdir="$(realpath "$0" | xargs dirname)"
 
 # Import the makerelease.lib
-# http://bues.ch/gitweb?p=misc.git;a=blob_plain;f=makerelease.lib;hb=HEAD
+# https://bues.ch/cgit/misc.git/tree/makerelease.lib
+die() { echo "$*"; exit 1; }
 for path in $(echo "$PATH" | tr ':' ' '); do
-	[ -f "$MAKERELEASE_LIB" ] && break
-	MAKERELEASE_LIB="$path/makerelease.lib"
+    [ -f "$MAKERELEASE_LIB" ] && break
+    MAKERELEASE_LIB="$path/makerelease.lib"
 done
 [ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
-
 
 hook_get_version()
 {
